1a3e9bee6fe179998b1228a811618359b1823285
mohamed elshafey <elshafeym170@gmail.com>
2023-11-07 12:01:06 +0200

    Adding Queue And Stack classes

---
+++ DataStructures/CQueue.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class CQueue<T> : IEnumerable<T>
+    {
+        List<T> list = new List<T>();
+
+        public CQueue(List<T> list, QueueIteratorBehavior queueIteratorBehavior = QueueIteratorBehavior.PeekWhenIterate)
+        {
+            this.list = list;
+            this.queueIteratorBehavior = queueIteratorBehavior;
+        }
+
+        public CQueue(CQueue<T>.QueueIteratorBehavior queueIteratorBehavior = QueueIteratorBehavior.PeekWhenIterate)
+        {
+            this.queueIteratorBehavior = queueIteratorBehavior;
+        }
+
+        public QueueIteratorBehavior queueIteratorBehavior { get; set; } = QueueIteratorBehavior.PeekWhenIterate;
+
+        public T Dequeue()
+        {
+            T item = Peek();
+            list.RemoveAt(0);
+            return item;
+        }
+
+        public T Peek()
+        {
+            return list[0];
+        }
+
+        public void Enqueue(T item)
+        {
+            list.Add(item);
+        }
+
+        public void Clear()
+        {
+            list.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return list.Contains(item);
+        }
+
+        public T[] ToArray()
+        {
+            return list.ToArray();
+        }
+
+        public List<T> ToList()
+        {
+            return list;
+        }
+
+        public T[] CopyTo(T[] arr, int index = 0)
+        {
+            for (int i = 0; i <= list.Count - 1 && index < arr.Length; i++)
+            {
+                arr[index] = list[i];
+                index++;
+            }
+            return arr;
+        }
+
+        public void PeekWhenIterate()
+        {
+            queueIteratorBehavior = QueueIteratorBehavior.PeekWhenIterate;
+        }
+        public void DequeueWhenIterate()
+        {
+            queueIteratorBehavior = QueueIteratorBehavior.DequeueWhenIterate;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new StackEnumerator(list, queueIteratorBehavior);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private class StackEnumerator : IEnumerator<T>
+        {
+            private List<T> list = new List<T>();
+            private int currentIndex = -1;
+            public QueueIteratorBehavior queueIteratorBehavior { get; set; } = QueueIteratorBehavior.PeekWhenIterate;
+            public StackEnumerator(List<T> list, QueueIteratorBehavior queueIteratorBehavior = QueueIteratorBehavior.PeekWhenIterate)
+            {
+                this.list = list;
+                this.queueIteratorBehavior = queueIteratorBehavior;
+            }
+
+            public T Current => list[currentIndex];
+
+            object IEnumerator.Current => Current;
+
+            public bool MoveNext()
+            {
+                if (queueIteratorBehavior == QueueIteratorBehavior.DequeueWhenIterate && currentIndex >= -1)
+                {
+                    list.RemoveAt(0);
+                    return currentIndex >= -1;
+                }
+                currentIndex++;
+                return currentIndex >= -1;
+            }
+
+            public void Reset()
+            {
+                currentIndex = -1;
+            }
+
+            public void Dispose()
+            {
+            }
+        }
+
+        public enum QueueIteratorBehavior
+        {
+            DequeueWhenIterate,
+            PeekWhenIterate
+        }
+    }
+
+}
---
+++ DataStructures/CStack.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public class CStack<T>:IEnumerable<T>
+    {
+        List<T> list = new List<T>();
+
+        public CStack(List<T> list, CStack<T>.IteratorBehavior iteratorBehavior = IteratorBehavior.PeekWhenIterate)
+        {
+            this.list = list;
+            this.iteratorBehavior = iteratorBehavior;
+        }
+
+        public CStack(CStack<T>.IteratorBehavior iteratorBehavior = IteratorBehavior.PeekWhenIterate)
+        {
+            this.iteratorBehavior = iteratorBehavior;
+        }
+
+        public IteratorBehavior iteratorBehavior { get; set; } = IteratorBehavior.PeekWhenIterate;
+
+        public T Pop()
+        {
+            T item = Peek();
+            list.RemoveAt(list.Count - 1);
+            return item;
+        } 
+
+        public T Peek()
+        {
+            return list[list.Count - 1];
+        }
+
+        public void Push(T item)
+        {
+            list.Add(item);
+        }
+
+        public void Clear()
+        {
+            list.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return list.Contains(item);
+        }
+
+        public T[] ToArray()
+        {
+            return list.ToArray();
+        }
+
+        public List<T> ToList()
+        {
+            return list;
+        }
+
+        public T[] CopyTo(T[] arr,int index = 0)
+        {
+            for(int i = list.Count - 1; i >= 0 && index < arr.Length; i--)
+            {
+                arr[index] = list[i];
+                index++;
+            }
+            return arr;
+        }
+
+        public void PeekWhenIterate()
+        {
+            iteratorBehavior = IteratorBehavior.PeekWhenIterate;
+        }
+        public void PopWhenIterate()
+        {
+            iteratorBehavior = IteratorBehavior.PopWhenIterate;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new StackEnumerator(list, iteratorBehavior);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private class StackEnumerator : IEnumerator<T>
+        {
+            private List<T> list = new List<T>();
+            private int currentIndex =  - 1;
+            public IteratorBehavior iteratorBehavior { get; set; } = IteratorBehavior.PeekWhenIterate;
+            public StackEnumerator(List<T> list, IteratorBehavior iteratorBehavior = IteratorBehavior.PeekWhenIterate)
+            {
+                this.list = list;
+                this.iteratorBehavior = iteratorBehavior;
+                currentIndex = list.Count - 1;
+            }
+
+            public T Current => list[currentIndex];
+
+            object IEnumerator.Current => Current;
+
+            public bool MoveNext()
+            {
+                if(iteratorBehavior == IteratorBehavior.PopWhenIterate && currentIndex >= 0)
+                {
+                    list.RemoveAt(currentIndex);
+                }
+                currentIndex--;
+                return currentIndex >= -1;
+            }
+
+            public void Reset()
+            {
+                currentIndex = list.Count - 1;
+            }
+
+            public void Dispose()
+            {
+            }
+        }
+
+        public enum IteratorBehavior
+        {
+            PopWhenIterate,
+            PeekWhenIterate
+        }
+    }
+}
